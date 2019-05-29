e93d293fae6513c5d396e3e690e8310d6b3631dd
Brandon Rose <rose.brandon.m@gmail.com>
2019-05-28 18:21:18 -0600

    Updates to model service to reflect integrations with user-spaced MINT catalogs

--- REST-Server/openapi_server/controllers/exploration_controller.py
+++ REST-Server/openapi_server/controllers/exploration_controller.py
@@ -9,7 +9,40 @@ from openapi_server.models.query import Query  # noqa: E501
 from openapi_server import util
 
 import requests
+import configparser
 
+import mint_client
+from mint_client.rest import ApiException
+
+# Load MINT configurations
+config = configparser.ConfigParser()
+config.read('config.ini')
+url = config['MINT']['URL']
+provenance_id = config['MINT']['PROVENANCE_ID']
+username = config['MINT']['USERNAME']
+password = config['MINT']['PASSWORD']
+
+# Authenticate with MINT Model Catalog (MCAT)
+configuration = mint_client.Configuration()
+api_instance = mint_client.UserApi()
+user = mint_client.User(username=username, password=password)
+
+# Authenticate with MINT Data Catalog (DCAT)
+resp = requests.get(f"{url}/get_session_token").json()
+api_key = resp['X-Api-Key']
+
+# Set DCAT request headers
+request_headers = {
+    'Content-Type': "application/json",
+    'X-Api-Key': api_key
+}
+
+try:
+    # Logs user into MINT
+    configuration.access_token = api_instance.login_user(username, password)
+    print("Log in success! Token: %s\n" % configuration.access_token)
+except ApiException as e:
+    print("Exception when calling UserApi->login_user: %s\n" % e)    
 
 def list_models_post():  # noqa: E501
     """Obtain a list of current models
@@ -19,15 +52,13 @@ def list_models_post():  # noqa: E501
 
     :rtype: List[str]
     """
-    params = (
-        ('endpoint', 'https://endpoint.mint.isi.edu/ds/query'),
-    )
-
-    response = requests.get('https://query.mint.isi.edu/api/mintproject/MINT-ModelCatalogQueries/getModels', params=params)
-    models_ = response.json()['results']['bindings']
-    models = [m['label']['value'] for m in models_]
-
-    return models
+    api_instance = mint_client.ModelApi(mint_client.ApiClient(configuration))
+    try:
+        api_response = api_instance.get_models(username=username)
+        models = [m.id for m in api_response]
+        return models
+    except ApiException as e:
+        return "Exception when calling ModelApi->get_models: %s\n" % e
 
 
 def model_config_model_name_get(ModelName):  # noqa: E501
@@ -40,14 +71,31 @@ def model_config_model_name_get(ModelName):  # noqa: E501
 
     :rtype: ModelConfig
     """
-    response = requests.get('https://query.mint.isi.edu/api/mintproject/MINT-ModelCatalogQueries/getModelConfigurations')
-    configs = []
-    for conf in response.json()['results']['bindings']:
-        if ModelName.lower() in conf.get('desc',{}).get('value',{}).lower():
-            out_conf = {'config': conf,
-                        'name': ModelName}
-            configs.append(out_conf)
-    return configs
+    # get model
+    try:
+        api_instance = mint_client.ModelApi(mint_client.ApiClient(configuration))
+        model = api_instance.get_model(ModelName, username=username)
+        versions = [v['id'] for v in model.has_software_version]
+
+        # for each model version, obtain configuration ids
+        configuration_ids = []
+        api_instance = mint_client.ModelversionApi(mint_client.ApiClient(configuration))
+        for v in versions:
+            version = api_instance.get_model_version(v, username=username)
+            c_ids = [c.id for c in version.has_configuration]
+            configuration_ids.extend(c_ids)
+
+        # get configurations
+        configurations = []
+        api_instance = mint_client.ModelconfigurationApi(mint_client.ApiClient(configuration))
+        for _id in configuration_ids:
+            config = api_instance.get_model_configuraton(_id, username=username)
+            configurations.append({'name': ModelName, 'config': config.to_dict()})
+
+        return configurations
+
+    except ApiException as e:
+        return "Exception when calling MINT: %s\n" % e
 
 def model_info_model_name_get(ModelName):  # noqa: E501
     """Get basic metadata information for a specified model.
@@ -59,20 +107,27 @@ def model_info_model_name_get(ModelName):  # noqa: E501
 
     :rtype: Model
     """
-    response = requests.get(f'https://api.models.mint.isi.edu/v0.0.2/model/{ModelName}')
-    model = response.json()
-    model['description'] = ''
-    model['maintainer'] = ''
-    model['name'] = model.pop('id')
-    model['category'] = model.pop('hasModelCategory')
-
-    return model
+    try:
+        api_instance = mint_client.ModelApi(mint_client.ApiClient(configuration))
+        api_response = api_instance.get_model(ModelName, username=username)
+        model = {
+                'name': api_response.id,
+                'description': api_response.description,
+                'maintainer': '',
+                'category': api_response.has_model_category,
+                'versions': [v['id'] for v in api_response.has_software_version]
+        }
+        return model
+    except ApiException as e:
+        return "Exception when calling ModelApi->get_model: %s\n" % e
 
 
 def model_io_post():  # noqa: E501
     """Obtain information on a given model&#39;s inputs or outputs.
 
     Submit a model name and receive information about the input or output files required by this model. # noqa: E501
+    Note that this includes all inputs and all outputs for a given model, irrespective of the 
+    configuration. In the future this could be subset to just a specific configuration.
 
     :param io_request: The name of a model and an IO type.
     :type io_request: dict | bytes
@@ -83,23 +138,46 @@ def model_io_post():  # noqa: E501
         io_request = IORequest.from_dict(connexion.request.get_json())  # noqa: E501
         name_ = io_request.name
         type_ = io_request.iotype
-        params = (
-            ('model', f"https://w3id.org/mint/instance/{name_}"),
-            ('endpoint', 'https://endpoint.mint.isi.edu/ds/query'),
-        )
 
-        response = requests.get('https://query.mint.isi.edu/api/mintproject/MINT-ModelCatalogQueries/getVariablePresentationsForModel', params=params)
-        var_rep = response.json()['results']['bindings'][0]
-        input_files = var_rep['input_files']['value'].split(', ')
-        output_files = var_rep['output_files']['value'].split(', ')
-        if type_ == 'input':
-            f_ = input_files
-        else:
-            f_ = output_files
-        files_variables = []
-        for f in f_:
-            files_variables.append(util._get_variables(f))
-        return files_variables
+        try:
+            # get model configuration ids
+            api_instance = mint_client.ModelApi(mint_client.ApiClient(configuration))
+            model = api_instance.get_model(name_, username=username)
+            versions = [v['id'] for v in model.has_software_version]
+            
+
+            # for each model version, obtain configuration ids
+            configuration_ids = []
+            api_instance = mint_client.ModelversionApi(mint_client.ApiClient(configuration))
+            for v in versions:
+                version = api_instance.get_model_version(v, username=username)
+                c_ids = [c.id for c in version.has_configuration]
+                configuration_ids.extend(c_ids)
+
+            # get IO
+            api_instance = mint_client.ModelconfigurationApi(mint_client.ApiClient(configuration))
+
+            inputs_outputs = []
+            if type_ == 'input':
+                for config_id in configuration_ids:
+                    api_response = api_instance.get_inputs_by_modelconfiguration(config_id, username=username)
+                    for io in api_response:
+                        io_ = util._parse_io(io, url, request_headers)
+                        if io_:
+                            inputs_outputs.append(io_)
+
+            elif type_ == 'output':
+                outputs = []
+                for config_id in configuration_ids:
+                    api_response = api_instance.get_outputs_by_modelconfiguration(config_id, username=username)
+                    for io in api_response:
+                        io_ = util._parse_io(io, url, request_headers)
+                        if io_:
+                            inputs_outputs.append(io_)
+            return inputs_outputs
+
+        except ApiException as e:
+            return "Exception when calling MINT API: %s\n" % e
 
 
 def search_post():  # noqa: E501
--- REST-Server/openapi_server/util.py
+++ REST-Server/openapi_server/util.py
@@ -4,6 +4,7 @@ import six
 import typing
 
 import requests
+from uuid import UUID
 
 def _deserialize(data, klass):
     """Deserializes dict, list, str into an object.
@@ -142,6 +143,33 @@ def _deserialize_dict(data, boxed_type):
             for k, v in six.iteritems(data)}
 
 
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+
+    Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+
+    Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+
+    Examples
+    --------
+    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
+    True
+    >>> is_valid_uuid('c9bf9e58')
+    False
+    """
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except:
+        return False
+
+    return str(uuid_obj) == uuid_to_test
+
 
 def _get_variables(file):
     """Obtain MINT variables for a given IO file"""
@@ -170,3 +198,39 @@ def _get_variables(file):
               'description': ''}
               
     return output
+
+def _parse_io(io, url, request_headers):
+    """Parse MINT input/output object into a dictionary"""
+    io_ = io.to_dict()
+
+    # only if ID is a valid UUID4 otherwise skip dataset
+    if is_valid_uuid(io_['id']):
+        io_['name'] = io_.pop('label')
+        io_['filetype'] = io_.pop('has_format')
+        io_.pop('type')
+        io_.pop('has_dimensionality')
+
+        # query DCAT for variable information related to dataset
+        q = {
+          "dataset_id": io_['id']
+        }
+
+        resp = requests.post(f"{url}/datasets/dataset_variables",
+                             headers=request_headers,
+                             json=q).json()
+
+        variables = resp['dataset']['variables']
+
+        
+        io_.pop('has_presentation')
+        io_['variables'] = []
+        for v in variables:
+            v['name'] = v.pop('variable_name')
+            v['id'] = v.pop('variable_id')
+            v['metadata'] = v.pop('variable_metadata')
+            # TODO: obtain standard name information
+            v['standard_name'] = 'TODO'
+            io_['variables'].append(v)
+        return io_    
+    else:
+        return None
