816a9a5b4dfafcd66e2acc4570bb7cea990a4d6d
Brandon Rose <rose.brandon.m@gmail.com>
2019-05-29 19:33:26 -0600

    Implemented working FSC model integration with MaaS

--- REST-Server/openapi_server/controllers/execution_controller.py
+++ REST-Server/openapi_server/controllers/execution_controller.py
@@ -6,12 +6,17 @@ from openapi_server.models.run_results import RunResults  # noqa: E501
 from openapi_server.models.run_status import RunStatus  # noqa: E501
 from openapi_server import util
 from openapi_server.kimetrica import KiController
+from openapi_server.fsc import FSCController
 
 import json
 from hashlib import sha256
 import docker
 import configparser
 import redis
+import boto3
+import botocore
+import logging
+logging.basicConfig(level=logging.INFO)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -22,6 +27,8 @@ r = redis.Redis(host=config['REDIS']['HOST'],
 
 client = docker.from_env()
 containers = client.containers
+
+available_models = ['malnutrition_model', 'fsc']
 
 def list_runs_model_name_get(ModelName):  # noqa: E501
     """Obtain a list of runs for a given model
@@ -53,26 +60,39 @@ def run_model_post():  # noqa: E501
     if connexion.request.is_json:
         model_config = ModelConfig.from_dict(connexion.request.get_json())  # noqa: E501
         model_config = model_config.to_dict()
-        
-        if model_config["name"] != "malnutrition_model":
-            return 'Model Not Found', 404, {'x-error': 'not found'}
+        model_name = model_config["name"]
 
-        # run the model        
-        kc = KiController(model_config)
-        model_container = kc.run_model()
+        if model_name.lower() not in available_models:
+            return 'Model Not Found', 404, {'x-error': 'not found'}
 
         # generate id for the model run
         run_id = sha256(json.dumps(model_config).encode('utf-8')).hexdigest()
 
+        if model_name.lower() == 'malnutrition_model':
+            # run the model        
+            kc = KiController(model_config)
+            model_container = kc.run_model()
+            stored = 1 # use binary for Redis
+            m = kc
+
+        elif model_name.lower() == 'fsc':
+            model_config['config']['run_id'] = run_id
+            fsc = FSCController(model_config['config'], config['FSC']['OUTPUT_PATH'])
+            model_container = fsc.run_model()
+            stored = 0 # use binary for Redis
+            m = fsc
+
         # push the id to the model's list of runs
-        r.sadd(model_config['name'], run_id)
+        r.sadd(model_name, run_id)
 
         # generate a key for the model run based on the run_id
-        run_obj = {'config': json.dumps(model_config),
+        run_obj = {'config': json.dumps(model_config['config']),
                    'status': 'PENDING',
                    'container': model_container.id,
-                   'bucket': kc.bucket,
-                   'key': kc.key}
+                   'bucket': m.bucket,
+                   'key': m.key,
+                   'stored': stored,
+                   'name': model_config['name']}
         r.hmset(run_id, run_obj)        
 
     return run_id
@@ -95,8 +115,10 @@ def run_results_run_idget(RunID):  # noqa: E501
     run = r.hgetall(RunID)
     status = run[b'status'].decode('utf-8')
     config = json.loads(run[b'config'].decode('utf-8'))
+    model_name = run[b'name'].decode('utf-8')
     output = ''
-    results = {'status': status, 'config': config, 'output': output}
+    output_config = {'config': config, 'name': model_name}
+    results = {'status': status, 'config': output_config, 'output': output}
 
     if status == 'SUCCESS':
         bucket = run[b'bucket'].decode('utf-8')
@@ -108,7 +130,7 @@ def run_results_run_idget(RunID):  # noqa: E501
         run_container = run[b'container']
         run_logs = run_container.logs().decode('utf-8')
         results['output'] = run_logs
-    
+    print(results)
     return results
 
 
@@ -124,27 +146,69 @@ def run_status_run_idget(RunID):  # noqa: E501
     """
     return update_run_status(RunID)
 
+
 def update_run_status(RunID):
     if not r.exists(RunID):
         return 'Run Not Found', 404, {'x-error': 'not found'}
 
     run = r.hgetall(RunID)
     run_container_id = run[b'container'].decode('utf-8')
+    model_name = run[b'name'].decode('utf-8')
     
     model_container = containers.get(run_container_id)
     model_container.reload()
     container_status = model_container.status    
 
-    success_msg = 'Model run: SUCCESS'
-    fail_msg = 'Model run: FAIL'
+
     run_logs = model_container.logs().decode('utf-8')
+    conf = json.loads(run[b'config'].decode('utf-8'))
+
+    # if Kimetrica malnutrition model
+    if model_name.lower() == 'malnutrition_model':
+        success_msg = 'Model run: SUCCESS'
+
+    # if FSC model
+    elif model_name.lower() == 'fsc':
+        success_msg = 'Output files stored to'
 
     status = 'PENDING'
     if container_status == 'exited':
         if success_msg in run_logs:
+            # if FSC we need to ensure results are stored to S3
+            # since Kimetrica model handles this within Docker directly
+            try:
+                store_results(RunID)
+            except:
+                return 'ERROR'
+
             r.hmset(RunID, {'status': 'SUCCESS'})
             status = 'SUCCESS'
-        elif success_msg in run_logs:
+        else:
             r.hmset(RunID, {'status': 'FAIL'})
             status = 'FAIL'
-    return status   
+    return status
+
+
+def store_results(RunID):
+    run = r.hgetall(RunID)
+    key = run[b'key'].decode('utf-8')
+    bucket = config['S3']['BUCKET']
+    model_config = json.loads(run[b'config'].decode('utf-8'))
+
+    # check if S3 key exists
+    s3 = boto3.resource('s3')
+    try:
+        s3.Object(bucket, key).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # The object does not exist.
+            try:
+                fsc = FSCController(model_config, config['FSC']['OUTPUT_PATH'])
+                fsc.storeResults() 
+            except Exception as e:
+                logging.error(e)
+        else:
+            logging.error('ERROR')
+    else:
+        # Key exists, do nothing
+        logging.info('Results already stored to S3')    
--- REST-Server/openapi_server/fsc.py
+++ REST-Server/openapi_server/fsc.py
@@ -3,27 +3,42 @@ import re
 import configparser
 import redis
 import os
+import shutil
+import time
+import logging
+import boto3
 
 class FSCController(object):
     """
     A controller to manage FSC model execution.
     """
 
-    def __init__(self, model_config):
+    def __init__(self, model_config, output_path):
         self.model_config = model_config
         self.client = docker.from_env()
         self.containers = self.client.containers
         self.fsc = 'fsc/latest'
+        self.result_path = output_path
+        self.result_name = self.model_config['run_id']               
         self.bucket = "world-modelers"
-        self.key = "results/fsc_model/outputs"
+        self.key = f"results/fsc_model/{self.result_name}.zip"
         self.entrypoint=f"Rscript /main/main.R \
                           {model_config['year']} \
                           {model_config['country']} \
                           {model_config['production_decrease']} \
                           {model_config['fractional_reserve_access']} \
-                           FSC_output"
-        self.volumes = {f'{os.getcwd()}/outputs': {'bind': '/outputs', 'mode': 'rw'}}
+                           {self.result_name}"
+        self.volumes = {self.result_path: {'bind': '/outputs', 'mode': 'rw'}}
 
+        logging.basicConfig(level=logging.INFO)
+        
+        # try: # try to remove any prior container named `fsc`
+        #     logging.info("Pruning old FSC containers...")
+        #     prior_container = self.containers.get('fsc')
+        #     removed = prior_container.remove()
+        #     logging.info("Removed prior FSC container.")
+        # except: # otherwise do nothing
+        #     logging.info("No prior container to remove.")
 
     def run_model(self):
         """
@@ -32,7 +47,7 @@ class FSCController(object):
         self.model = self.containers.run(self.fsc, 
                                          volumes=self.volumes, 
                                          entrypoint=self.entrypoint,
-                                         name='fsc',
+                                         # name='fsc',
                                          detach=True)
         return self.model
 
@@ -44,3 +59,20 @@ class FSCController(object):
         model_logs = self.model.logs()
         model_logs_decoded = model_logs.decode('utf-8')
         return model_logs_decoded
+    
+    
+    def storeResults(self):
+        result = f"{self.result_path}/{self.result_name}"
+        exists = os.path.isdir(result)
+        logging.info(exists)
+        if exists:
+            shutil.make_archive(result, 'zip', result)            
+            s3 = boto3.client('s3')
+            s3.upload_file(f"{result}.zip", 
+                           self.bucket, 
+                           f"{self.key}", 
+                           ExtraArgs={'ACL':'public-read'})
+            logging.info(f'Results stored at : https://s3.amazonaws.com/world-modelers/{self.key}')
+            return "SUCCESS"
+        else:
+            return result
