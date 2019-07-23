188bb3a8bc4f05d3676ebf739593def9fd16602a
Justin Gawrilow <justin.gawrilow@gmail.com>
2019-07-23 10:08:38 -0400

    Update maas_install.sh

--- FSC-Integration/maas_install.sh
+++ FSC-Integration/maas_install.sh
@@ -1,8 +1,8 @@
 # Check out FSC Model and switch to docker branch
 git clone https://github.com/mjpuma/FSC-WorldModelers.git
+cd FSC-WorldModelers
 git checkout docker
 
 # Build container used in maas.
 # THIS IS A RATHER LARGE IMAGE AND YOU SHOULD MAKE SURE YOU HAVE 10GB+ IN SPACE
-cd FSC-WorldModelers
 docker build -t fsc/latest .
