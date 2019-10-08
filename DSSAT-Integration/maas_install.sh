f55c829867192c8d1331998f15b3e67678404054
Brandon Rose <rose.brandon.m@gmail.com>
2019-10-08 15:49:25 -0500

    Updated maas_install.sh for DSSAT to appropriately build Docker data volume 'ethdata'

--- DSSAT-Integration/maas_install.sh
+++ DSSAT-Integration/maas_install.sh
@@ -11,8 +11,8 @@ wget https://world-modelers.s3.amazonaws.com/data/ssddata-20191003.tar.xz
 wget https://raw.githubusercontent.com/WorldModelers/ModelService/master/DSSAT-Integration/et_docker.json
 
 # create data volume
-docker run -v /data/ETH -v $PWD:/userdata --name ethdata debian:stable-slim /bin/bash
-docker run --rm --volumes-from ethdata -v ${PWD}:/userdata debian:stable-slim bash -c "apt-get update && apt-get install xz-utils && cd /data/base && tar xJvf /userdata/basedata-20191003.tar.xz && cd /data/ETH && tar xJvf /userdata/ethdata-20191003.tar.xz && cd /data/SS && tar xJvf /userdata/ssddata-20191003.tar.xz"
+docker run -v /data -v $PWD:/userdata --name ethdata debian:stable-slim /bin/bash
+docker run --rm --volumes-from ethdata -v ${PWD}:/userdata debian:stable-slim bash -c "apt-get update && apt-get install xz-utils && echo 'making directories...' && mkdir /data/base && mkdir /data/ETH && mkdir /data/SSD && echo '...directories built' && cd /data/base && tar xJvf /userdata/basedata-20191003.tar.xz && cd /data/ETH && tar xJvf /userdata/ethdata-20191003.tar.xz && cd /data/SSD && tar xJvf /userdata/ssddata-20191003.tar.xz"
 
 # pull Docker image
 docker pull cvillalobosuf/dssat-pythia:latest
