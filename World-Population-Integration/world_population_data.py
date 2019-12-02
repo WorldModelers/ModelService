cdb7a1a2cfc101ee1d93c5e37c7e9a7642a2a9d6
Brandon Rose <rose.brandon.m@gmail.com>
2019-12-02 16:06:18 -0600

    Updated world_population_data.py to upload raw .tif files to appropriate location on S3

--- World-Population-Integration/world_population_data.py
+++ World-Population-Integration/world_population_data.py
@@ -1,3 +1,4 @@
+import boto3
 import os
 import redis
 import configparser
@@ -12,9 +13,15 @@ r = redis.Redis(host=config['REDIS']['HOST'],
                 port=config['REDIS']['PORT'],
                 db=config['REDIS']['DB'])
 
-bucket = 'world-modelers'
+profile = "wmuser"
+bucket = "world-modelers"
 
-def gen_run(year, output):
+session = boto3.Session(profile_name=profile)
+s3 = session.resource("s3")
+s3_client = session.client("s3")
+s3_bucket= s3.Bucket(bucket)
+
+def gen_run(year, input_file, output):
     model_name = 'world_population_africa'
     model_config = {
                     'config': {
@@ -40,6 +47,11 @@ def gen_run(year, output):
     run_obj['config']['run_id'] = run_id
     run_obj['config'] = json.dumps(run_obj['config'])
     
+    # Upload file to S3
+    print(f"Uploading {run_obj['key']}...")
+    s3_bucket.upload_file(input_file, run_obj['key'])
+
+    # Create Redis object
     r.hmset(run_id, run_obj)
 
 
@@ -61,5 +73,6 @@ if __name__ == "__main__":
 
     for year in years:
         # Generate merged runs
+        input_file = f"Africa_1km_Population/AFR_PPP_{year}_adj_v2.tif"
         output = "world_population_africa_{}.tif".format(year)
-        gen_run(year, output)
+        gen_run(year, input_file, output)
