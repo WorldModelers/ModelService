22d9e890c2b681af87e6a15ab397e5256f4804f7
Brandon Rose <rose.brandon.m@gmail.com>
2020-01-03 11:17:22 -0600

    Added processing script for G-Range model

--- CSIRO-Integration/G-Range_processing.py
+++ CSIRO-Integration/G-Range_processing.py
@@ -79,38 +79,36 @@ def sortOD(od):
             res[k] = v
     return res                                
 
-def process_crops_(crops_, scen, crop_type, season_type, scenarios, apsim):
+def process_herbage(herbage, scen, scenarios, grange):
     """
-    Primary function for processing each crop type/season/scenario combo
+    Primary function for processing grange
     """
 
     # subset for the correct scenario
-    crops_ = crops_[crops_['scenario'] == scen]
+    herbage = herbage[herbage['scenario'] == scen]
 
-    crops_['geometry'] = crops_.apply(lambda x: Point(x.longitude, x.latitude), axis=1)
+    herbage['geometry'] = herbage.apply(lambda x: Point(x.longitude, x.latitude), axis=1)
 
     # obtain scenario parameters
     params = scenarios[scenarios['scenario']==scen].iloc[0].to_dict()
-    params['crop'] = crop_type
-    params['season'] = season_type
 
     run_id, model_config, run_obj = gen_run(model_name, params)
 
     # generate temp CSV and push it to S3
-    crops_.to_csv("tmp.csv", index=False)
+    herbage.to_csv("tmp.csv", index=False)
     s3_bucket.upload_file("tmp.csv", run_obj['key'], ExtraArgs={'ACL':'public-read'})
 
     # Add metadata object to DB
     meta = Metadata(run_id=run_id, 
                     model=model_name,
                     raw_output_link= f"https://model-service.worldmodelers.com/results/{model_name}_results/{run_id}.csv",
-                    run_label=crops_.description.iloc[0],
+                    run_label=herbage.description.iloc[0],
                     point_resolution_meters=25000)
     db_session.add(meta)
     db_session.commit()
 
     # Add parameters to DB
-    for param in apsim['parameters']:
+    for param in grange['parameters']:
         # ensure that no null parameters are stored
         if not pd.isna(params[param['name']]):
             if param['metadata']['type'] == 'ChoiceParameter':
@@ -132,7 +130,7 @@ def process_crops_(crops_, scen, crop_type, season_type, scenarios, apsim):
             db_session.add(param)
             db_session.commit()
         
-    gdf = gpd.GeoDataFrame(crops_)
+    gdf = gpd.GeoDataFrame(herbage)
     gdf = gpd.sjoin(gdf, admin2, how="left", op='intersects')
     gdf['run_id'] = run_id
     gdf['model'] = model_name
@@ -165,8 +163,8 @@ s3_bucket= s3.Bucket(bucket)
 scenarios = pd.read_csv('Scenarios.csv')
 grids = pd.read_csv('Experiment 2020-01 - Gridcell Centre Points.csv')
 
-with open('../metadata/models/APSIM-model-metadata.yaml', 'r') as stream:
-    apsim = yaml.safe_load(stream)
+with open('../metadata/models/G-Range-model-metadata.yaml', 'r') as stream:
+    grange = yaml.safe_load(stream)    
 
 admin2 = gpd.read_file("gadm36_ETH_shp/gadm36_ETH_2.shp")
 admin2['country'] = admin2['NAME_0']
@@ -178,92 +176,50 @@ admin2['GID_1'] = admin2['GID_1'].apply(lambda x: x.split("_")[0])
 
 eth = cascaded_union(admin2.geometry)
 
-crops = pd.read_csv('C2-P2 APSIM-GRange Results v01/Cropping_Grid_Backcast_Experiment_2020-01.csv')
-crops_lt = pd.read_csv('C2-P2 APSIM-GRange Results v01/Cropping_Grid_LTMean_Experiment_2020-01.csv')
+herbage = pd.read_csv('C2-P2 APSIM-GRange Results v01/G-Range_Grid_Month_Experiment_2020-01.csv')
 
 # obtain lat/lon from grid file
-crops = crops.merge(grids, how='left', left_on='gridcell_id', right_on='CellId')
-crops_lt = crops_lt.merge(grids, how='left', left_on='gridcell_id', right_on='CellId')
+herbage = herbage.merge(grids, how='left', left_on='gridcell_id', right_on='CellId')
+
+# Set up datetime field
+herbage['datetime'] = herbage.month.apply(lambda x: datetime(year=int(x.split('-')[0]),month=int(x.split('-')[1]),day=1))
 
 # add in parameters from scenarios dataframe
-crops = crops.merge(scenarios, on='scenario', how='left', suffixes=(False, False))
+herbage = herbage.merge(scenarios, on='scenario', how='left', suffixes=(False, False))
 
-crops_lt = crops_lt.merge(scenarios, on='scenario', how='left', suffixes=(False, False))
-
-model_name = apsim['id']
-crop_param = [i for i in apsim['parameters'] if i['name']=='crop'][0]
-season_param = [i for i in apsim['parameters'] if i['name']=='season'][0]
+model_name = grange['id']
 
 outputs = {}
-for o in apsim['outputs']:
+for o in grange['outputs']:
     outputs[o['name']] = o
     
-scenario_list = crops.scenario.unique()
-scenario_list_lt = crops_lt.scenario.unique()
+scenario_list = herbage.scenario.unique()
 
 param_cols = list(scenarios.columns)
-base_cols = ['gridcell_id', 'cropping_year','latitude','longitude']
-base_cols_lt = ['gridcell_id','latitude','longitude']
+base_cols = list(herbage.columns)
 
 ##################################################
 ##################################################
 
 if __name__ == "__main__":
+# process G-Range backcast results
+    for scen in scenario_list:
 
-    # process backcast results
-    for season_type in season_param['metadata']['choices']:
-        for crop_type in crop_param['metadata']['choices']:
-            for scen in scenario_list:
+        # subset for the correct scenario
+        herbage_ = herbage[herbage['scenario'] == scen]
 
-                # select the correct yield columns for crop/season and rename them
-                yield_col = f"yield_{season_type}_{crop_type}"
-                anomaly_col = f"rel_anomaly_{yield_col}"
-                cols = param_cols + base_cols + [yield_col, anomaly_col]
-                crops_ = crops[cols]
-                crops_ = crops_.rename(columns={yield_col:'yield',anomaly_col:'rel_anomaly_yield'})
-                crops_['datetime'] = crops_.cropping_year.apply(lambda x: datetime(year=x,month=1,day=1))
-                
-                # subset for the correct scenario
-                crops_ = crops_[crops_['scenario'] == scen]
-                
-                # drop rows where yield fields are NA
-                crops_ = crops_.dropna(subset=['yield','rel_anomaly_yield'])
-                
-                gdf, run_id = process_crops_(crops_, scen, crop_type, season_type, scenarios, apsim)
+        # drop rows where yield fields are NA
+        # herbage = herbage.dropna(subset=['yield','rel_anomaly_yield'])
 
-                print(f"Processing {crop_type} for {season_type} season with run_id {run_id}")
-                    
-                for feature in ['yield','rel_anomaly_yield']:
-                    gdf_ = gdf
-                    gdf_['feature_name'] = feature
-                    gdf_['feature_value'] = gdf_[feature]
-                    gdf_['feature_description'] = outputs[feature]['description']
-                    
-                    db_session.bulk_insert_mappings(Output, gdf_.to_dict(orient="records"))
-                    db_session.commit()    
-        
-    # process LT forecasts
-    for season_type in season_param['metadata']['choices']:
-        for crop_type in crop_param['metadata']['choices']:
-            for scen in scenario_list_lt:
-                
-                # select the correct yield columns for crop/season and rename them
-                yield_col = f"lt_yield_{season_type}_{crop_type}"
-                cols = param_cols + base_cols_lt + [yield_col]
-                crops_ = crops_lt[cols]
-                crops_ = crops_.rename(columns={yield_col:'lt_yield'})
+        gdf, run_id = process_herbage(herbage_, scen, scenarios, grange)
 
-                # drop rows where yield fields are NA
-                crops_ = crops_.dropna(subset=['lt_yield'])            
-                
-                gdf, run_id = process_crops_(crops_, scen, crop_type, season_type, scenarios, apsim)
-                
-                print(f"Processing {crop_type} for {season_type} season with run_id {run_id}")
+        print(f"Processing G-Range with run_id {run_id}")
 
-                gdf_ = gdf
-                gdf_['feature_name'] = feature
-                gdf_['feature_value'] = gdf_['lt_yield']
-                gdf_['feature_description'] = outputs[feature]['description']
+        for feature in [i['name'] for i in grange['outputs']]:
+            gdf_ = gdf
+            gdf_['feature_name'] = feature
+            gdf_['feature_value'] = gdf_[feature]
+            gdf_['feature_description'] = outputs[feature]['description']
 
-                db_session.bulk_insert_mappings(Output, gdf_.to_dict(orient="records"))
-                db_session.commit()    
+            db_session.bulk_insert_mappings(Output, gdf_.to_dict(orient="records"))
+            db_session.commit()    
